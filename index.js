b4fc110fe9e9fa95d8cd3108873a5da65f15c62b
Collins Kipkemoi <kipkemoicollis@gmail.com>
2025-04-18 23:11:40 +0300

    Employ operators to perform arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Random (greater than 0)
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Mod (remainder should be 4)
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+
+// Max (should return 20 as the highest)
+const max = Math.max(1, 3, 5, 10, 20);
